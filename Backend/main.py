2569561ddf18f1d93c0fbee7d1ba40f5140acfb6
Banhivaroy <roybanhiva@gmail.com>
2026-09-01 23:34:19 +0530

    cors error fixed

--- Backend/main.py
+++ Backend/main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+        "https://leetcode-find-iota.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
